b33f0f117bf6d2a5309a7575f6e0c0f2372fedee
TiyaniK <98035695+TiyaniK@users.noreply.github.com>
2022-10-08 12:03:09 +0200

    fix

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
-/** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://www.boilingsports.co.za/',
-    generateRobotsTxt: true, // (optional)
-    // ...other options
-  }
+  siteUrl: "https://www.boilingsports.co.za/",
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+
+};
