9054717da7be1038062c23c1a4e96f22f5d86ffd
TiyaniK <98035695+TiyaniK@users.noreply.github.com>
2022-10-08 10:28:58 +0200

    fix

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,4 +1,4 @@
-/** @type {import('next-sitemap').IConfig} */
+
 module.exports = {
     siteUrl: process.env.SITE_URL || 'https://www.boilingsports.co.za/',
     generateRobotsTxt: true, // (optional)
