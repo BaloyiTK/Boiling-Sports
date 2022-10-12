66fa97e35f51658eb1676e8b09e04bc3da408581
TiyaniK <98035695+TiyaniK@users.noreply.github.com>
2022-10-12 17:19:13 +0200

    some changes

--- services/index.js
+++ services/index.js
@@ -5,7 +5,7 @@ const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
 export const getPosts = async () => {
   const query = gql`
     query MyQuery {
-      postsConnection (orderBy: createdAt_DESC, last: 100) {
+      postsConnection (orderBy: createdAt_DESC, last:1000) {
         edges {
           cursor
           node {
