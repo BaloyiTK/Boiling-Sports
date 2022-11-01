0a5971e1fe7cde26e7eb452093d451274e4b7431
TiyaniK <98035695+TiyaniK@users.noreply.github.com>
2022-11-01 09:03:01 +0200

    updates

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -49,6 +49,7 @@ const PostDetail = ({ post }) => {
             </div>
           </div>
           <h1 className="mb-8 text-3xl font-semibold">{post.title}</h1>
+          
           <div>
             <ReactMarkdown>{post.content.markdown}</ReactMarkdown>
           </div>
